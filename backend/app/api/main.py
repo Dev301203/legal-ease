3e976e5e7fbb0cac825bcf28eaf0298ac352439e
Tyler Biswurm <biswurmt@cs.toronto.edu>
2025-10-26 10:24:12 -0400

    Fix backend export issue

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter
 
-from app.api.routes import audio_models, legal, tree_generation, utils, web_app
+from app.api.routes import audio_models, legal, tree_generation, web_app
+from app.api.routes.utils import router as utils_router
 from app.core.config import settings
 
 api_router = APIRouter()
 
-api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
+api_router.include_router(utils_router, prefix="/utils", tags=["utils"])
 api_router.include_router(audio_models.router)
 api_router.include_router(tree_generation.router)
 api_router.include_router(web_app.router)
